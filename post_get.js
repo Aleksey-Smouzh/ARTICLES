26806369d8cbb542a1bab1132381b4ff0a5cdd38
Aleksey-Smouzh <xmz@tut.by>
2022-01-22 22:30:19 +0300

    22/01/22

--- post_get.js
+++ post_get.js
@@ -5,17 +5,47 @@ const getResourse = async (url) => {
   }
   return await response.json();
 };
-const data = getResourse("https://61d496de8df81200178a8d99.mockapi.io/api/v1/user").then((data) => console.log(data))
+// const data = getResourse(
+//   "https://61d496de8df81200178a8d99.mockapi.io/api/v1/user"
+// ).then((data) => console.log(data));
 
-// const baseUrl = 'https://61d496de8df81200178a8d99.mockapi.io/api/v1/user';
+let addMessageDate = document.querySelector("#date_of"),
+  addMessageDeadline = document.querySelector("#deadline"),
+  addMessageStatus = document.querySelector("#status"),
+  addMessageTitle = document.querySelector("#title_articl"),
+  addMessageLoadText = document.querySelector("#text"),
+  addMessageLoadFoto = document.querySelector("#foto"),
+  addButton = document.querySelector("#send"),
+  todo = document.querySelector("#todo");
 
-// export const createTask = taskData =>
-// fetch(baseUrl, {
-//   method: 'POST',
-//   headers: {
-//     'content_Type': 'application/json;charset=utf-8'
-//   },
-//   body: JSON.stringify(taskData)
-// });
+let todoList = [];
 
+addButton.addEventListener("click", function () {
+  let newTodo = {
+    todoData: addMessageDate.value,
+    todoDeadline: addMessageDeadline.value,
+    todoTitle: addMessageTitle.value,
+    todoLoadText: addMessageLoadText.value,
+    todoLoadFoto: addMessageLoadFoto.value,
+    checked: false,
+  };
 
+  todoList.push(newTodo);
+  displayMessages();
+  localStorage.setItem('todo', JSON.stringify(todoList));
+});
+function displayMessages() {
+  let displayMessage = "";
+
+  todoList.forEach(function(item, i) {
+    displayMessage += `
+    <li>
+    <input type='checkbox' id='item_${i}' ${item.checked ? "checked" : ""}>
+    <label for='item_${i}'>${item.todo}</label>
+    </li>
+    `;
+    todo.innerHTML = displayMessage;
+
+    console.log("newTodo: ", displayMessage);
+  });
+}
